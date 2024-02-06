86787f8cf5e63608d536a5adb83e51fd0e6ae797
unknown <pabloagustinjesus@gmail.com>
2024-02-05 23:13:43 -0300

    Listo hasta el while

--- 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
+++ 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Pablo
+apellido: Jesus
 ---
 TP: While_elecciones_paso
 ---
@@ -35,7 +35,47 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        flag=True
+        flagnombre=True
+        flagedad=True
+        flagvotos=True
+        minimo=25555555555
+        maximo=0
+        contador=0
+        sumaedad=0
+        sumavotos=0
+        while(flag):
+            while(flagnombre):
+                nombre = prompt("Nombre", "Ingrese nombre: ")
+                if(nombre!=None and nombre!=""):
+                    contador+=1
+                    flagnombre=False
+                else:
+                    promedio=sumaedad/contador
+                    alert("Resultados", "El candidato mas votado fue: " + nombremaximo + "\nEl candidato con menos votos fue: "
+                    + nombreminimo + " con " + str(edad) + " años." + "\nEl promedio de edades de los candidatos fue de: "
+                    + str(promedio) + "\nEl total de los votos emitidos fue de: " + str(sumavotos))
+                    return 0
+            while(flagedad):
+                edad = int(prompt("Edad", "Ingrese edad (mayor a 25)"))
+                if(edad>=25):
+                    sumaedad+=edad
+                    flagedad=False
+            while(flagvotos):
+                votos = int(prompt("Votos", "Ingrese la cantidad de votos"))
+                if(votos>=0):
+                    sumavotos+=votos
+                    flagvotos=False
+            if(votos<minimo):
+                nombreminimo=nombre
+                edadminimo=edad
+                minimo=votos
+            if(votos>maximo):
+                nombremaximo=nombre
+                maximo=votos
+            flagnombre=True
+            flagvotos=True
+            flagedad=True
 
 
 if __name__ == "__main__":
